c0d04507e81244ac81f57d7d2a6e082a727623c2
Alina Rouvinen <98087771+Alina-enni@users.noreply.github.com>
2022-01-27 14:09:40 +0200

    Step into the right direction

--- search_engine.py
+++ search_engine.py
@@ -7,9 +7,7 @@ documents = ["This is a silly example",
 
 cv = CountVectorizer(lowercase=True, binary=True)
 sparse_matrix = cv.fit_transform(documents)
-
 sparse_td_matrix = sparse_matrix.T.tocsr()
-
 t2i = cv.vocabulary_  # shorter notation: t2i = term-to-index
 
 # Operators and/AND, or/OR, not/NOT become &, |, 1 -
@@ -33,21 +31,23 @@ def test_query(query):
     print("Matching:", eval(rewrite_query(query))) # Eval runs the string as a Python command
     print()
 
-test_query("example AND NOT nothing")
-test_query("NOT example OR great")
-test_query("( not example or great ) and nothing") # ... or all small letters
-test_query("not example and not nothing")
+def print_contents(query):
+    hits_matrix = eval(rewrite_query(query))
+    print("Matching documents as vector (it is actually a matrix with one single row):", hits_matrix)
+    print("The coordinates of the non-zero elements:", hits_matrix.nonzero())
+    hits_list = list(hits_matrix.nonzero()[1])
+    print(hits_list)
+    for i, doc_idx in enumerate(hits_list):
+        print("Matching doc #{:d}: {:s}".format(i, documents[doc_idx]))
 
-hits_matrix = eval(rewrite_query("NOT example OR great"))
-print("Matching documents as vector (it is actually a matrix with one single row):", hits_matrix)
-print("The coordinates of the non-zero elements:", hits_matrix.nonzero())
-hits_list = list(hits_matrix.nonzero()[1])
-print(hits_list)
-for i, doc_idx in enumerate(hits_list):
-    print("Matching doc #{:d}: {:s}".format(i, documents[doc_idx]))
-
-queryinput = 0
+queryinput = '0'
 while queryinput != '':
     queryinput = input("Type your query: ")
+    print(t2i)
     test_query(queryinput)
+    print_contents(queryinput)
 
+# Loop through dictionary and if word is not in the dictionary, do something
+
+if queryinput == '':
+    print("Goodbye!")
