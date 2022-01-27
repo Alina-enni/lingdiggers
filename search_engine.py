da88cd5496666d172fcb81089fe79271ec7fdcc3
Alina Rouvinen <98087771+Alina-enni@users.noreply.github.com>
2022-01-27 12:59:38 +0200

    Update script

--- search_engine.py
+++ search_engine.py
@@ -10,21 +10,38 @@ def main():
     cv = CountVectorizer(lowercase=True, binary=True)
     sparse_matrix = cv.fit_transform(documents)
 
-    print("Term-document matrix: (?)\n")
-    print(sparse_matrix)
-
     dense_matrix = sparse_matrix.todense()
-
-    print("Term-document matrix: (?)\n")
-    print(dense_matrix)
 
     td_matrix = dense_matrix.T   # .T transposes the matrix
 
-    print("Term-document matrix:\n")
-    print(td_matrix)
-
     t2i = cv.vocabulary_  # shorter notation: t2i = term-to-index
-    print("Query: example")
-    print(td_matrix[t2i["example"]])
+
+    # Operators and/AND, or/OR, not/NOT become &, |, 1 -
+    # Parentheses are left untouched
+    # Everything else interpreted as a term and fed through td_matrix[t2i["..."]]
+
+    d = {"and": "&", "AND": "&",
+        "or": "|", "OR": "|",
+        "not": "1 -", "NOT": "1 -",
+        "(": "(", ")": ")"}          # operator replacements
+
+    def rewrite_token(t):
+        return d.get(t, 'sparse_td_matrix[t2i["{:s}"]].todense()'.format(t)) # Make retrieved rows dense
+        
+    def rewrite_query(query): # rewrite every token in the query
+        return " ".join(rewrite_token(t) for t in query.split())
+
+    def test_query(query):
+        print("Query: '" + query + "'")
+        print("Rewritten:", rewrite_query(query))
+        print("Matching:", eval(rewrite_query(query))) # Eval runs the string as a Python command
+        print()
+
+    test_query("example AND NOT nothing")
+    test_query("NOT example OR great")
+    test_query("( NOT example OR great ) AND nothing") # AND, OR, NOT can be written either in ALLCAPS
+    test_query("( not example or great ) and nothing") # ... or all small letters
+    test_query("not example and not nothing")
+
 
 main()
