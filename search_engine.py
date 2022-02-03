547b12d6951f77bce1f0959c257a10bac4178711
Alina Rouvinen <98087771+Alina-enni@users.noreply.github.com>
2022-02-03 16:33:35 +0200

    Fixed the erroneous printing of an empty document
    The script now produces the correct output for searching "NOT Angola" = 92 matching documents (instead of 93 like previously). My fix is not very sophisticated as I was not able to fix the underlying issue, but using conditionals, it is easy to remove the empty match from the end of the output using 'brute force'.

--- search_engine.py
+++ search_engine.py
@@ -14,7 +14,7 @@ documents = [remove_html.sub(' ', a).strip() for a in documents]
 cv = CountVectorizer(lowercase=True, binary=True, analyzer="word", token_pattern=r"(?u)\b\w+\b")
 sparse_matrix = cv.fit_transform(documents)
 sparse_td_matrix = sparse_matrix.T.tocsr()
-t2i = cv.vocabulary_  # shorter notation: t2i = term-to-index
+t2i = cv.vocabulary_  # Shorter notation: t2i = term-to-index
 
 # Operators and/AND, or/OR, not/NOT become &, |, 1 -
 # Parentheses are left untouched
@@ -22,12 +22,12 @@ t2i = cv.vocabulary_  # shorter notation: t2i = term-to-index
 d = {"and": "&", "AND": "&",
     "or": "|", "OR": "|",
     "not": "1 -", "NOT": "1 -",
-    "(": "(", ")": ")"}          # operator replacements
+    "(": "(", ")": ")"}          # Operator replacements
 
 def rewrite_token(t):
     return d.get(t, 'sparse_td_matrix[t2i["{:s}"]].todense()'.format(t)) # Make retrieved rows dense
     
-def rewrite_query(query): # rewrite every token in the query
+def rewrite_query(query): # Rewrite every token in the query
     return " ".join(rewrite_token(t) for t in query.split())
 
 def test_query(query):
@@ -36,17 +36,29 @@ def test_query(query):
     print("Matching:", eval(rewrite_query(query))) # Eval runs the string as a Python command
     print()
 
-def print_contents(query):
+def print_contents(query, boolean):
     if query != "UKN":
         hits_matrix = eval(rewrite_query(query))
         hits_list = list(hits_matrix.nonzero()[1])
-        if len(hits_list) == 1:
+        if len(hits_list) == 1 and boolean == 0:
             print(len(hits_list) ,"document found")
-        else:
+        elif len(hits_list) > 1 and boolean == 0:
             print(len(hits_list) ,"documents found")
-        print('')
-        for i, doc_idx in enumerate(hits_list, start=1):
-            print("Matching doc #{:d}: {:s}".format(i, textwrap.shorten(documents[doc_idx], width=100)))
+        elif len(hits_list) == 1 and boolean == 1:
+            print((len(hits_list)-1) ,"document found")
+        elif len(hits_list) > 1 and boolean == 1:
+            print((len(hits_list)-1),"documents found")
+            
+        print()
+        if boolean == 0:
+            for i, doc_idx in enumerate(hits_list, start=1):
+                print("Matching doc #{:d}: {:s}".format(i, textwrap.shorten(documents[doc_idx], width=100)))
+
+        elif boolean == 1:
+            hits_list = hits_list[:-1]
+            for i, doc_idx in enumerate(hits_list, start=1):
+                print("Matching doc #{:d}: {:s}".format(i, textwrap.shorten(documents[doc_idx], width=100)))
+
     elif query == "UKN":
         print("Sorry, that document does not exist in the collection.")
 
@@ -55,37 +67,42 @@ while queryinput != []:
     queryinput = input("Type your query: ").casefold()
     queryinput = queryinput.split()    # Split query in case it contains multiple terms
     if len(queryinput) == 1:    # If query consists of only one term, operate on that
+        boolean = 0
         queryinput = ' '.join(map(str, queryinput))
         if queryinput in t2i.keys():
-            print_contents(queryinput)
+            print_contents(queryinput, boolean)
             print()
 
         elif queryinput not in t2i.keys() and queryinput != "":
             queryinput = "UKN"
             t2i[queryinput] = 0
-            print_contents(queryinput)
+            print_contents(queryinput, boolean)
             print()
 
     elif len(queryinput) > 1:       # If query consists of multiple terms, e.g. "NOT example"
         i = 0
-        operators = 0
+        boolean = 0
+        operators = 0               # Counter variable for keeping track of operators in the query
         multiquery = []
         while i < len(queryinput): 
             if queryinput[i] in d.keys():           # Check if term found in operators
                 multiquery.append(queryinput[i])
                 operators += 1
-            elif queryinput[i] in t2i.keys():       # Check if term found in the documents
+                if queryinput[i] == "not" or queryinput == "NOT":
+                    boolean = 1
+            elif queryinput[i] in t2i.keys():       # Check if term found in documents
                 multiquery.append(queryinput[i])
             elif queryinput[i] not in t2i.keys():   # If there's an unknown term
                 t2i["UKN"] = 0
                 multiquery.append("UKN")
             i += 1
         
+        # Query only run if it consists of a single search term or multiple separated by an operator
         if operators == 0 and len(queryinput) > 1:
             print("Please use an operator to separate multiple search terms. The accepted operators are AND and OR.")
         elif operators != 0 or len(queryinput) == 1:
             multiquery = ' '.join(map(str, multiquery))
-            print_contents(multiquery)
+            print_contents(multiquery, boolean)
 
 if queryinput == []:
     print()
