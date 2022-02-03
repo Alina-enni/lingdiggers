121e72f940a47e9831e090f85bd2fe55ee34a9fc
Alina Rouvinen <98087771+Alina-enni@users.noreply.github.com>
2022-02-03 15:34:04 +0200

    Error handling for searching multiple terms without a separating operator
    There is now a print statement for when the user enters a multiple-term query without a separating operator such as "in Angola". The script is not run and the user is told to separate search terms with "and" or "or".

--- search_engine.py
+++ search_engine.py
@@ -2,8 +2,8 @@ from sklearn.feature_extraction.text import CountVectorizer
 import re
 import textwrap
 
-# for Alina: /Users/alina/Documents/GitHub/lingdiggers/
-f = open("100articles.txt", encoding="utf-8")
+# Filepath for Alina: /Users/alina/Documents/GitHub/lingdiggers/
+f = open("/Users/alina/Documents/GitHub/lingdiggers/100articles.txt", encoding="utf-8")
 op = f.read()
 f.close()
 op = re.sub(r'\n', r'', op)
@@ -39,10 +39,7 @@ def test_query(query):
 def print_contents(query):
     if query != "UKN":
         hits_matrix = eval(rewrite_query(query))
-        #print("Matching documents as vector (it is actually a matrix with one single row):", hits_matrix)
-        #print("The coordinates of the non-zero elements:", hits_matrix.nonzero())
         hits_list = list(hits_matrix.nonzero()[1])
-        #print(hits_list)
         if len(hits_list) == 1:
             print(len(hits_list) ,"document found")
         else:
@@ -60,32 +57,35 @@ while queryinput != []:
     if len(queryinput) == 1:    # If query consists of only one term, operate on that
         queryinput = ' '.join(map(str, queryinput))
         if queryinput in t2i.keys():
-            #test_query(queryinput)
             print_contents(queryinput)
             print()
 
         elif queryinput not in t2i.keys() and queryinput != "":
             queryinput = "UKN"
             t2i[queryinput] = 0
-            #test_query(queryinput)
             print_contents(queryinput)
             print()
 
     elif len(queryinput) > 1:       # If query consists of multiple terms, e.g. "NOT example"
         i = 0
+        operators = 0
         multiquery = []
         while i < len(queryinput): 
             if queryinput[i] in d.keys():           # Check if term found in operators
                 multiquery.append(queryinput[i])
+                operators += 1
             elif queryinput[i] in t2i.keys():       # Check if term found in the documents
                 multiquery.append(queryinput[i])
             elif queryinput[i] not in t2i.keys():   # If there's an unknown term
                 t2i["UKN"] = 0
                 multiquery.append("UKN")
             i += 1
-
-        multiquery = ' '.join(map(str, multiquery))
-        print_contents(multiquery)
+        
+        if operators == 0 and len(queryinput) > 1:
+            print("Please use an operator to separate multiple search terms. The accepted operators are AND and OR.")
+        elif operators != 0 or len(queryinput) == 1:
+            multiquery = ' '.join(map(str, multiquery))
+            print_contents(multiquery)
 
 if queryinput == []:
     print()
