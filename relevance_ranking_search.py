2fafbfb352c2dc5aee25148aff1da997e0a5ecaa
miglamigla <98089940+miglamigla@users.noreply.github.com>
2022-02-06 23:13:41 +0200

    #15 Some updates to the engine

--- relevance_ranking_search.py
+++ relevance_ranking_search.py
@@ -69,70 +69,84 @@ def print_contents(query, boolean):
     elif query == "UKN":
         print("Sorry, that document does not exist in the collection.")
 
-search_type = 0
-while search_type != 3:
-    search_type = int(input("\nType 1 for Boolean search, 2 for tf-idf based search or 3 to quit: "))
+def boolean_search():
+    queryinput = input("Type your query: ").casefold()
+    queryinput = queryinput.split()  # Split query in case it contains multiple terms
+    if len(queryinput) == 1:  # If query consists of only one term, operate on that
+        boolean = 0
+        queryinput = ' '.join(map(str, queryinput))
+        if queryinput in t2i.keys():
+            print_contents(queryinput, boolean)
+            print()
+        elif queryinput not in t2i.keys() and queryinput != "":
+            queryinput = "UKN"
+            t2i[queryinput] = 0
+            print_contents(queryinput, boolean)
+            print()
 
-    # tf-idf based search
-    if search_type == 2:
-        queryinput = input("Type your query: ")
-        query_vec5 = tfv5.transform([queryinput]).tocsc() # CSC: compressed sparse column format
-        hits = np.dot(query_vec5, sparse_matrix)
-        print()
-        print("The matching documents are:", hits.nonzero()[1])
-        print()
-        print("The scores of the documents are:", np.array(hits[hits.nonzero()])[0])
-        print()
+    elif len(queryinput) > 1:  # If query consists of multiple terms, e.g. "NOT example"
+        i = 0
+        boolean = 0
+        operators = 0  # Counter variable for keeping track of operators in the query
+        multiquery = []
+        while i < len(queryinput):
+            if queryinput[i] in d.keys():  # Check if term found in operators
+                multiquery.append(queryinput[i])
+                operators += 1
+                if queryinput[i] == "not" or queryinput == "NOT":
+                    boolean = 1
+            elif queryinput[i] in t2i.keys():  # Check if term found in documents
+                multiquery.append(queryinput[i])
+            elif queryinput[i] not in t2i.keys():  # If there's an unknown term
+                t2i["UKN"] = 0
+                multiquery.append("UKN")
+            i += 1
+
+        # Query only run if it consists of a single search term or multiple separated by an operator
+        if operators == 0 and len(queryinput) > 1:
+            print("Please use an operator to separate multiple search terms. The accepted operators are AND and OR.")
+        elif operators != 0 or len(queryinput) == 1:
+            multiquery = ' '.join(map(str, multiquery))
+            print_contents(multiquery, boolean)
+
+def tf_idf_search(t2i):
+    queryinput = input("Type your query: ")
+    query_vec5 = tfv5.transform([queryinput]).tocsc()  # CSC: compressed sparse column format
+    hits = np.dot(query_vec5, sparse_matrix)
+    if queryinput in t2i.keys():
+        if len(hits.nonzero()[1]) == 1:
+            print("\n{} document found".format(len(hits.nonzero()[1])))
+        elif len(hits.nonzero()[1]) > 1:
+            print("{} documents found".format(len(hits.nonzero()[1])))
+
+        print("\nThe matching documents are:", hits.nonzero()[1])
+        print("\nThe scores of the documents are:", np.array(hits[hits.nonzero()])[0], "\n")
 
         ranked_scores_and_doc_ids = sorted(zip(np.array(hits[hits.nonzero()])[0], hits.nonzero()[1]), reverse=True)
-        for score, i in ranked_scores_and_doc_ids: 
-            print("The score of", queryinput, "is {:.4f} in document: {:s}".format(score, textwrap.shorten(documents[i], width=100)))
+        for score, i in ranked_scores_and_doc_ids:
+            print("The score of", queryinput,
+                "is {:.4f} in document: {:s}".format(score, textwrap.shorten(documents[i], width=100)))
         print()
-        
-    # Boolean search
-    elif search_type == 1:
-        queryinput = input("Type your query: ").casefold()
-        queryinput = queryinput.split()    # Split query in case it contains multiple terms
-        if len(queryinput) == 1:    # If query consists of only one term, operate on that
-            boolean = 0
-            queryinput = ' '.join(map(str, queryinput))
-            if queryinput in t2i.keys():
-                print_contents(queryinput, boolean)
-                print()
-            elif queryinput not in t2i.keys() and queryinput != "":
-                queryinput = "UKN"
-                t2i[queryinput] = 0
-                print_contents(queryinput, boolean)
-                print()
+    else:
+        print("Sorry, that document does not exist in the collection.")
 
-        elif len(queryinput) > 1:       # If query consists of multiple terms, e.g. "NOT example"
-            i = 0
-            boolean = 0
-            operators = 0               # Counter variable for keeping track of operators in the query
-            multiquery = []
-            while i < len(queryinput): 
-                if queryinput[i] in d.keys():           # Check if term found in operators
-                    multiquery.append(queryinput[i])
-                    operators += 1
-                    if queryinput[i] == "not" or queryinput == "NOT":
-                        boolean = 1
-                elif queryinput[i] in t2i.keys():       # Check if term found in documents
-                    multiquery.append(queryinput[i])
-                elif queryinput[i] not in t2i.keys():   # If there's an unknown term
-                    t2i["UKN"] = 0
-                    multiquery.append("UKN")
-                i += 1
-                
-            # Query only run if it consists of a single search term or multiple separated by an operator
-            if operators == 0 and len(queryinput) > 1:
-                print("Please use an operator to separate multiple search terms. The accepted operators are AND and OR.")
-            elif operators != 0 or len(queryinput) == 1:
-                multiquery = ' '.join(map(str, multiquery))
-                print_contents(multiquery, boolean)
-    
-    elif search_type != 1 and search_type != 2 and search_type != 3:
-        print("\nThat is not a valid choice!")
 
-if search_type == 3:
-    print()
-    print("Quitting the program. Goodbye!")
+def main():
+    search_type = 0
+    while search_type != 3:
+        search_type = int(input("\nType 1 for Boolean search, 2 for tf-idf based search or 3 to quit: "))
+
+        if search_type == 2:
+            tf_idf_search(t2i)
+
+        elif search_type == 1:
+            boolean_search()
+
+        elif search_type != 1 and search_type != 2 and search_type != 3:
+            print("\nThat is not a valid choice!")
+
+    if search_type == 3:
+        print()
+        print("Quitting the program. Goodbye!")
+
+main()
