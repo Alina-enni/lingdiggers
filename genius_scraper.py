31ccd5277b239305a55149079a899f6feb0c918f
miglamigla <98089940+miglamigla@users.noreply.github.com>
2022-02-23 14:03:19 +0200

    Random song additions

--- genius_scraper.py
+++ genius_scraper.py
@@ -11,7 +11,7 @@ GENIUS_API_TOKEN='OK93fmBOPEHDihuH2xD5IloNlps1sofo_FFX3viQGxSbvbBFOlM9nke5jc9D4r
 from lyricsgenius import Genius
 
 file = open("lyrics2.txt", "a")
-artists = ['Coldplay', 'Tori Amos', 'Michael Jackson', 'Beatles', 'Blur', 'Taylor Swift', 'Blue October', 'Porcupine Tree', 'Muse', 'Annie Lennox', 'Frank Sinatra']
+artists = ['Coldplay', 'Tori Amos', 'Michael Jackson', 'Beatles', 'Blur', 'Taylor Swift', 'Blue October', 'Porcupine Tree', 'Muse', 'Annie Lennox', 'Frank Sinatra', 'David Bowie', 'Elton John', 'Bombay Bicycle Club', 'The Kinks', 'Radiohead', 'Bananarama', 'Franz Ferdinand']
 genius = Genius(GENIUS_API_TOKEN, skip_non_songs=False, remove_section_headers=True)
 
 def get_lyrics(arr, k):
