4cab229ab1232393b5e8cc002b194a941e7df52c
Alina Rouvinen <98087771+Alina-enni@users.noreply.github.com>
2022-02-24 12:19:44 +0200

    Remove Boolean search parts and fix empty results

--- flaskdemo.py
+++ flaskdemo.py
@@ -19,21 +19,16 @@ documents = op.split(r'<|endoftext|>')
 # tf-idf based search
 tfv5 = TfidfVectorizer(lowercase=True, sublinear_tf=True, use_idf=True, norm="l2")
 sparse_matrix = tfv5.fit_transform(documents).T.tocsr() # CSR: compressed sparse row format => order by terms
+vocab = tfv5.vocabulary_
 
-# Boolean search
-cv = CountVectorizer(lowercase=True, binary=True, analyzer="word", token_pattern=r"(?u)\b\w+\b")
-sparse_matrix2 = cv.fit_transform(documents)
-sparse_td_matrix2 = sparse_matrix2.T.tocsr()
-t2i = cv.vocabulary_  # Shorter notation: t2i = term-to-index
-
-def tf_idf_search(tfv5, t2i, query):
+def tf_idf_search(tfv5, vocab, query):
 
     query = query.split()  # Split query in case it contains multiple terms
     if len(query) == 1:  # If query consists of only one term, operate on that
         query = ' '.join(map(str, query))
         if query != "":
             searchlist = []
-            for word in t2i.keys():  # looping through all possible words in doc
+            for word in vocab.keys():  # looping through all possible words in doc
                 if re.search('^({}.+|{})'.format(query, query), word, re.IGNORECASE):  # if it finds words that start with the query...
                     searchlist.append(word)  # ...it appends them to our new list
             if searchlist != []:
@@ -61,15 +56,11 @@ def tf_idf_search(tfv5, t2i, query):
                             matches.append(working_doc[index - 50: index + 50])
                         else:
                             continue
-                #for score, i in ranked_scores_and_doc_ids:
-                #    print("The score of", query,
-                #        "is {:.4f} in document #{:d}: {:s}".format(score, i, textwrap.shorten(documents[i], width=100)))
-
                 return ranked_scores_and_doc_ids, hits, total_docs, matching_docs, queryinput, matches
-            else:
-                print("Sorry, no matches found in the collection.")
         else:
-            print("Sorry, that document does not exist in the collection.")
+            return None
+
+
     elif len(query) > 1:  # If query consists of multiple terms
         query = ' '.join(map(str, query))
         if query != "":
@@ -88,7 +79,7 @@ def tf_idf_search(tfv5, t2i, query):
 
             return ranked_scores_and_doc_ids, hits, total_docs, matching_docs, matches
         else:
-            print("Sorry, that document does not exist in the collection.")
+            None
 
 #Function search() is associated with the address base URL + "/search"
 @app.route('/search')
@@ -103,15 +94,19 @@ def search():
     queryinput = []
     matches = []
 
-        #If query exists (i.e. is not None)
-    if query:
-        #Look at each entry in the example data
-        ranked_scores_and_doc_ids = tf_idf_search(tfv5, t2i, query)[0]
-        hits = tf_idf_search(tfv5, t2i, query)[1]
-        total_docs = tf_idf_search(tfv5, t2i, query)[2]
-        matching_docs = tf_idf_search(tfv5, t2i, query)[3]
-        queryinput = tf_idf_search(tfv5, t2i, query)[4]
-        matches = tf_idf_search(tfv5, t2i, query)[5]
+    results = tf_idf_search(tfv5, vocab, query)
+    if results == None:
+        ranked_scores_and_doc_ids = []
+        hits = []
+        total_docs = "0"
+        matching_docs = "None"
+        queryinput = "None"
+    else:
+        ranked_scores_and_doc_ids = results[0]
+        hits = results[1]
+        total_docs = results[2]
+        matching_docs = results[3]
+        queryinput = results[4]
 
     #Render index.html with matches variable
     return render_template('index.html', ranked_scores_and_doc_ids=ranked_scores_and_doc_ids, hits=hits, total_docs=total_docs, matching_docs=matching_docs, documents=documents, queryinput=queryinput, matches=matches)
