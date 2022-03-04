ccf1b75c4bf160915f332171465d5da734c51c65
miglamigla <98089940+miglamigla@users.noreply.github.com>
2022-03-04 14:42:49 +0200

    #27 Updates to genius_scraper and lyrics2

--- genius_scraper.py
+++ genius_scraper.py
@@ -10,28 +10,32 @@ GENIUS_API_TOKEN='OK93fmBOPEHDihuH2xD5IloNlps1sofo_FFX3viQGxSbvbBFOlM9nke5jc9D4r
 
 from lyricsgenius import Genius
 
-file = open("lyrics2.txt", "w", encoding="utf-8")
-artists = ['Coldplay', 'Tori Amos', 'Michael Jackson', 'Beatles', 'Blur',
+file = open("lyrics2.txt", "a", encoding="utf-8")
+artists = ['Coldplay', 'Tori Amos', 'Blur', 'Beatles', 'Michael Jackson', 'Queen',
            'Taylor Swift', 'Blue October', 'Porcupine Tree', 'Muse', 'Annie Lennox',
            'Frank Sinatra', 'David Bowie', 'Elton John', 'Bombay Bicycle Club', 'The Kinks',
-           'Radiohead', 'Bananarama', 'Franz Ferdinand', 'Kate Bush', 'Mos Def', 'Queen', 'Fleetwood Mac']
+           'Bananarama', 'Franz Ferdinand', 'Kate Bush', 'Mos Def', 'Fleetwood Mac', 'The Weeknd',
+           'Imagine Dragons', 'Radiohead', 'Billie Eilish', 'Jackson 5', 'Chet Baker']
 genius = Genius(GENIUS_API_TOKEN, skip_non_songs=False, remove_section_headers=True)
 
-def get_lyrics(arr, k):
+def get_lyrics(artists, k):
+    artist = input("Name of artist to add to lyrics file:")
     c = 0
-    for name in arr:
+    if artist not in artists:
         try:
-            songs = (genius.search_artist(name, max_songs=k)).songs
+            songs = (genius.search_artist(artist, max_songs=k)).songs
             s = [song.lyrics for song in songs]
             for i in s:
-                file.write(name)
+                file.write(artist)
                 file.write("\n")
                 file.write(i)
                 file.write("\n \n   <|endoftext|>   \n \n")
             c += 1
             print(f"Songs grabbed:{len(s)}")
         except:
-            print(f"some exception at {name}: ", c)
+            print(f"some exception at {artist}: ", c)
+    else:
+        print("We already have that!")
 
 get_lyrics(artists, 5)
 
